36cf0c9d13bf03de7ec4eead07e2b05f0a1823ee
matt-zhrani <ahmed.mm.alzhrani@gmail.com>
2017-02-03 06:10:30 -0600

    Initial commit

---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,71 @@
+/**
+ * Created by ahmed on 2/3/2017.
+ */
+package com.chess.engine.board;
+
+import com.chess.engine.piece.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i=0; i< 64; i++){
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public Tile(int tileCoordinate){
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+
+        EmptyTile(final int coordinate){
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile{
+
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int coordinate, Piece pieceOnTile){
+            super(coordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
---
+++ src/com/chess/engine/piece/Piece.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ahmed on 2/3/2017.
+ */
+package com.chess.engine.piece;
+public class Piece {
+}
