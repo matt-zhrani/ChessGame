765b62674189720a2b381c658533870294cd8a2e
matt-zhrani <ahmed.mm.alzhrani@gmail.com>
2017-02-07 05:53:08 -0600

    calculated edge cases for knight moves; added a BoardUtils class to keep track of particular tiles; built the Move class to address normal and attack moves

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,7 +1,35 @@
 package com.chess.engine.board;
 
+import com.chess.engine.pieces.Piece;
+
 /**
  * Created by ahmed on 2/4/2017.
  */
-public class Move {
+public abstract class Move {
+
+    final Board board;
+    final Piece movedPiece;
+    final int destinationCoordinate;
+
+    private Move(final Board board, final Piece piece, final int destinationCoordinate){
+        this.board = board;
+        this.movedPiece = piece;
+        this.destinationCoordinate = destinationCoordinate;
+    }
+
+    public static final class MajorMove extends Move{
+
+        public MajorMove(final Board board, final Piece piece, final int destinationCoordinate) {
+
+            super(board, piece, destinationCoordinate);
+        }
+    }
+
+    public static final class AttackMove extends Move{
+        final Piece attackedPiece;
+        public AttackMove(final Board board, final Piece piece, final int destinationCoordinate, final Piece attackedPieces) {
+            super(board, piece, destinationCoordinate);
+            this.attackedPiece = attackedPieces;
+        }
+    }
 }
--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -2,12 +2,15 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import static com.chess.engine.board.Move.*;
 
 /**
  * Created by ahmed on 2/4/2017.
@@ -21,26 +24,49 @@ public class Knight extends Piece {
     }
 
     @Override
-    public List<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(final Board board) {
 
-        int candidateDestinationCoordinate;
-        List<Move> legalMoves = new ArrayList<>();
-        for(final int currentCandidate : CANDIDATE_MOVE_COORDINATES){
-            candidateDestinationCoordinate = this.piecePosition + currentCandidate;
-            if(true/*isValidTileCoordinate*/){
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+            if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                    isSecondColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                    isSeventhColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                    isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)){
+                continue;
+            }
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
                 final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
                 if(!candidateDestinationTile.isTileOccupied()){
-                    legalMoves.add(new Move());
+                    legalMoves.add(new MajorMove(board, this,candidateDestinationCoordinate));
                 }
                 else{
                     final Piece pieceAtDestination = candidateDestinationTile.getPiece();
                     final Alliance pieceAlliance = pieceAtDestination.pieceAlliance;
                     if(this.pieceAlliance != pieceAlliance){
-                        legalMoves.add(new Move());
+                        legalMoves.add(new AttackMove(board, this
+                        , candidateDestinationCoordinate, pieceAtDestination));
                     }
                 }
             }
         }
         return ImmutableList.copyOf(legalMoves);
     }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -17 || candidateOffset == -10 || candidateOffset == 6 || candidateOffset == 15);
+    }
+
+    private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -10 || candidateOffset == 6);
+    }
+
+    private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -6 || candidateOffset == 10);
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -15 || candidateOffset == -6 || candidateOffset == 10 || candidateOffset == 17);
+    }
+
 }
